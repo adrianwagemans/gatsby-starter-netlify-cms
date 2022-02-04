eada74efc6ca1f02b84d218296dead8fbe15a8a2
adrianwagemans <adrianwagemans@gmail.com>
2022-02-04 19:18:54 +0100

    impressum

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -109,6 +109,19 @@ export default class Index extends React.Component {
             </div>
           </div>
         </section>
+        <div className='message'>
+          <div class='message-body'>
+            <h1>Impressum</h1>
+            <p>
+              Für einen Gewerbetreibenden, der nicht im Handelsregister
+              eingetragen ist, könnte das Impressum beispielsweise wie folgt
+              aussehen: Bernd Schmidt Lindenhof 29 77933 Lahr Telefon:
+              07821/29513 E-Mail: info@bigfoot-wanderungen.com
+              Umsatzsteuer-Identifikationsnummer gem. § 27a UStG: DE 10350/01122
+              Inhaltlich Verantwortlicher: Bernd Schmidt (Anschrift s.o.)
+            </p>
+          </div>
+        </div>
       </Layout>
     );
   }
